fbf1ad6fb191c3f28a1b53f48585da0e78626073
ADMIN <ADMIN@DESKTOP-OSDHN47>
2022-02-21 11:46:06 +0530

    demo

--- consulekart/src/main/java/consulekart/consulekart/App.java
+++ consulekart/src/main/java/consulekart/consulekart/App.java
@@ -13,7 +13,7 @@ public class App
     	Product p1=new Product("16GB pendrive", "Sandisk", 20, new String[] {"Black","Red"}, 250);
     	Product p2=new Product("Airdopes", "Boat", 180, new String[] {"White","Blue","Red"}, 999);
     	Product p3=new Product("15AC2022 Laptop", "Dell", 45, new String[] {"Grey","Silver","Black"}, 87600);
-        CommerceController controller=new CommerceController();
+//        CommerceController controller=new CommerceController();
         System.out.println(Arrays.toString(controller.available()));
 //        System.out.println(controller.newStock(p1));System.out.println(controller.newStock(p2));
 //        System.out.println(controller.newStock(p3));
